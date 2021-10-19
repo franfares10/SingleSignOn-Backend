fa598e4ce441f16e79a7d087d13cb84176eba17c
Fran Fares <franciscofares23@gmail.com>
2021-10-19 14:59:19 -0300

    Users collection added

---
+++ models/User.model.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    email: String,
+    tenant: String,
+    name: String,
+    last_name: String,
+    admin: Boolean
+})
+
+var Users = mongoose.model('users', UserSchema, 'users');
+
+module.exports = Users;
